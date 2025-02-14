3a4ee03aa9d5da0b6d3a7efd1cdd64d574e8f87d
AndresArtiga333 <andresaartiga057@gmail.com>
2025-02-14 11:22:03 -0600

    Se termino el modulo dl administrador de gestion de usuarios

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,12 +1,36 @@
+import bcrypt from "bcrypt";
 import Usuarios from "./user.model.js"
+
+export const agregarUsuario = async (req, res) =>{
+    try{
+        const data = req.body;
+            const saltos = bcrypt.genSaltSync(10);
+            const encryptedPassword = bcrypt.hashSync(data.contra, saltos)
+            data.contra = encryptedPassword
+        
+            const user = await Usuarios.create(data)
+        
+            return res.status(201).json({
+                message: "El usuario ha sido registrado",
+                nombre: user.nombre,
+                correo: user.correo
+            });
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al agregar el usuario",
+            error: err.message
+        })
+    }
+}
 
 export const actualizarUsuarios = async (req, res) =>{
     try{
-        const {uid} = req.params;
+        const {usuario} = req;
         const data = req.body;
 
         const usuarioActualizado = await Usuarios.findOneAndUpdate(
-        { _id: uid, rol: { $ne: "ADMIN" } }, {$set: data}, { new: true }
+         usuario, {$set: data}, { new: true }
         );
 
         if (!usuarioActualizado) {
@@ -29,23 +53,79 @@ export const actualizarUsuarios = async (req, res) =>{
     }
 }
 
-export const eliminarUsuario = async (req, res) => {
+export const actualizarModoAdmin = async (req, res) =>{
+    try{
+        const{uid} = req.params;
+        const data = req.body;
+        const adminActualizar = await Usuarios.findOneAndUpdate(
+            { _id: uid, rol: { $ne: "ADMIN" } }, {$set: data}, { new: true }
+            );
+
+            if (!adminActualizar) {
+                return res.status(403).json({
+                    success: false,
+                    message: "No se puede editar a los administradores o el usuario no existe"
+                });
+            }
+
+        return res.status(200).json({
+            success: true,
+            message: "Usuario",
+            adminActualizar
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al actualizar el usuario",
+            error: err.message
+        })
+    }
+}
+
+export const eliminarUsuarioModoAdmin = async (req, res) => {
     try{
         const { uid } = req.params
         
-        const user = await Usuarios.findByIdAndUpdate(uid, {status: false}, {new: true})
+        const usuarioParaEliminar = await Usuarios.findOneAndUpdate(
+            { _id: uid, rol: { $ne: "ADMIN" } }, {status: false}, { new: true }
+            );
 
-        if (!user || user.rol === "ADMIN") {
-            return res.status(403).json({
+        if (!usuarioParaEliminar) {
+            return res.status(404).json({
                 success: false,
-                message: "No se puede editar a los administradores o el usuario no existe"
+                message: "Usuario no encontrado"
             });
         }
 
         return res.status(200).json({
             success: true,
             message: "Usuario eliminado",
-            user
+            usuarioParaEliminar
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al eliminar el usuario",
+            error: err.message
+        })
+    }
+}
+
+export const eliminarUsuario = async (req, res) =>{
+    try{
+        const {usuario} = req;
+        const eliminarUsuario = await Usuarios.findOneAndUpdate(usuario, {status: false}, {new: true})
+        if(!eliminarUsuario){
+            return res.status(404).json({
+                success: false,
+                message: "Usuario no encontrado"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Usuario eliminado",
+            eliminarUsuario
         })
     }catch(err){
         return res.status(500).json({
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { actualizarUsuarios, eliminarUsuario } from "./user.controller.js";
-import { actualizarUsuarioValidator, eliminarUsuarioValidator} from "../middlewares/user-validator.js";
+import { agregarUsuario, actualizarUsuarios, eliminarUsuario, actualizarModoAdmin, eliminarUsuarioModoAdmin } from "./user.controller.js";
+import { crearUsuarioValidator ,actualizarUsuarioValidator, eliminarUsuarioValidator,eliminarUsuarioModoAdminValidator, actualizarAdminValidator} from "../middlewares/user-validator.js";
 
 const router = Router()
 
-router.put ("/actualizarUsuario/:uid", actualizarUsuarioValidator, actualizarUsuarios)
+router.post("/agregarUsuario", crearUsuarioValidator, agregarUsuario)
 
-router.patch("/eliminarUsuario/:uid", eliminarUsuarioValidator, eliminarUsuario)
+router.put ("/actualizarUsuario/", actualizarUsuarioValidator, actualizarUsuarios)
+
+router.put("/actualizarModoAdmin/:uid", actualizarAdminValidator, actualizarModoAdmin )
+
+router.patch("/eliminarUsuarioModoAdmin/:uid", eliminarUsuarioModoAdminValidator, eliminarUsuarioModoAdmin)
+
+router.patch("/eliminarUsuario/", eliminarUsuarioValidator, eliminarUsuario)
 
 export default router
